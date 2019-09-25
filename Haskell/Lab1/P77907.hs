0fa690e9f15020e9b53de8bebb8f3afd467ece2c
Ineskay <victordivi98@gmail.com>
2019-09-25 08:09:58 +0200

    Fix old quickFib (0 case)

--- Haskell/Lab1/P77907.hs
+++ Haskell/Lab1/P77907.hs
@@ -31,17 +31,27 @@ slowFib n
   |n==1 = 1
   |otherwise = slowFib (n-1) + slowFib (n-2)
 
+-- quickFib :: Int -> Int
+-- quickFib n = fst (auxFib n)
+--     where
+--       auxFib :: Int -> (Int, Int)
+--       auxFib 0 = (0, 1)
+--       auxFib n 
+--         | even n = (f, f1)
+--         | otherwise = (f1, f + f1)
+--         where
+--           (a, b) = auxFib (div n 2)
+--           f = a * (b * 2 - a)
+--           f1 = a * a + b * b
+
 quickFib :: Int -> Int
-quickFib n = fst (auxFib n)
+quickFib n = snd (auxFib n)
     where
       auxFib :: Int -> (Int, Int)
-      auxFib 0 = (0, 1)
-      auxFib n 
-        | even n = (f, f1)
-        | otherwise = (f1, f + f1)
+      auxFib 0 = (0, 0)
+      auxFib 1 = (0, 1)
+      auxFib n = (s, f + s)
         where
-          (a, b) = auxFib (div n 2)
-          f = a * (b * 2 - a)
-          f1 = a * a + b * b
+          (f, s) = auxFib (n-1)
     
 
