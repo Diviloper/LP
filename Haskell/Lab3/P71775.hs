b1fb887573acbf5fddb54f655debfb95b8dd7bae
Ineskay <victordivi98@gmail.com>
2019-09-25 18:50:39 +0200

    replace local function with anonymous function in pam2

--- Haskell/Lab3/P71775.hs
+++ Haskell/Lab3/P71775.hs
@@ -5,9 +5,7 @@ pam :: [Int] -> [Int -> Int] -> [[Int]]
 pam m fs = map (flip map m) fs
 
 pam2 :: [Int] -> [Int -> Int] -> [[Int]]
-pam2 m fs = map apply m
-    where
-        apply x = [f x | f <- fs]
+pam2 m fs = map (\x -> [f x | f <- fs]) m
 
 filterFoldl :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
 filterFoldl p f x m = foldl f x $ filter p m
