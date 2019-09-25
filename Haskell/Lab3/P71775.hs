ab7103241ce8c66fea375dbbb921e8fab12f9c8f
Ineskay <victordivi98@gmail.com>
2019-09-25 18:47:42 +0200

    Add second exercise of Lab3

---
+++ Haskell/Lab3/P71775.hs
@@ -0,0 +1,22 @@
+countIf :: (Int -> Bool) -> [Int] -> Int
+countIf p x = length $ filter p x
+
+pam :: [Int] -> [Int -> Int] -> [[Int]]
+pam m fs = map (flip map m) fs
+
+pam2 :: [Int] -> [Int -> Int] -> [[Int]]
+pam2 m fs = map apply m
+    where
+        apply x = [f x | f <- fs]
+
+filterFoldl :: (Int -> Bool) -> (Int -> Int -> Int) -> Int -> [Int] -> Int
+filterFoldl p f x m = foldl f x $ filter p m
+
+insert :: (Int -> Int -> Bool) -> [Int] -> Int -> [Int]
+insert p [] x = [x]
+insert p (m:ms) x 
+    | p m x = m : insert p ms x
+    | otherwise = [x, m] ++ ms
+
+insertionSort :: (Int -> Int -> Bool) -> [Int] -> [Int]
+insertionSort p ms = foldl (insert p) [] ms
